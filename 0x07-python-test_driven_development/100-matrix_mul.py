6953ec70a1f757fce215b79bc192b117071768b5
kareembadrsaber <kimobadrsaber@gmail.com>
2024-01-08 01:41:02 -0800

    test

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,65 +1,24 @@
 #!/usr/bin/python3
-"""Module matrix_mul
-Multiplies two matrices and returns the result.
-"""
-
-
 def matrix_mul(m_a, m_b):
-    """Return the matrix resulting from
-    the multiplication of m_a and m_b."""
+   """Return the matrix resulting of
+   the multiplication of m_a and m_b."""
 
-    if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
+   if not all(isinstance(x, list) for x in m_a):
+       raise TypeError("m_a must be a list of lists")
+   if not all(isinstance(x, list) for x in m_b):
+       raise TypeError("m_b must be a list of lists")
 
-    for x in m_a:
-        if type(x) is not list:
-            raise TypeError("m_a must be a list of lists")
-    for x in m_b:
-        if type(x) is not list:
-            raise TypeError("m_b must be a list of lists")
+   if not m_a or not m_b:
+       raise ValueError("Neither m_a nor m_b can be empty")
 
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
+   if not all(all(isinstance(x, (int, float)) for x in row) for row in m_a):
+       raise TypeError("All elements in m_a should be integers or floats")
+   if not all(all(isinstance(x, (int, float)) for x in row) for row in m_b):
+       raise TypeError("All elements in m_b should be integers or floats")
 
-    for row in m_a:
-        for x in row:
-            if type(x) is not int and type(x) is not float:
-                raise TypeError("m_a should contain only integers or floats")
-    for row in m_b:
-        for x in row:
-            if type(x) is not int and type(x) is not float:
-                raise TypeError("m_b should contain only integers or floats")
+   if len(m_a[0]) != len(m_b):
+       raise ValueError("The number of columns in m_a must match the number of rows in m_b")
 
-    row_len = []
-    for row in m_a:
-        row_len.append(len(row))
-    if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_a must be of the same size")
-    row_len = []
-    for row in m_b:
-        row_len.append(len(row))
-    if not all(elem == row_len[0] for elem in row_len):
-        raise TypeError("each row of m_b must be of the same size")
+   result = [[sum(a*b for a, b in zip(X_row,Y_col)) for Y_col in zip(*m_b)] for X_row in m_a]
 
-    a_col = 0
-    for col in m_a[0]:
-        a_col += 1
-    b_row = 0
-    for row in m_b:
-        b_row += 1
-
-    if a_col != b_row:
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    result = [[0 for x in range(len(m_b[0]))] for y in range(len(m_a))]
-
-    for i in range(len(m_a)):
-        for j in range(len(m_b[0])):
-            for k in range(len(m_b)):
-                result[i][j] += m_a[i][k] * m_b[k][j]
-
-    return result
+   return result
